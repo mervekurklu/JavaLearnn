524a9be97062076278dbf35c03cc76b1360d5101
mervekurklu <114594189+mervekurklu@users.noreply.github.com>
2022-10-26 13:48:17 +0300

    denemeler

---
+++ src/j01_Variables/HelloWorld.java
@@ -0,0 +1,35 @@
+package j01_Variables;
+
+public class HelloWorld {
+
+
+    public static void main(String[] args) {
+        //System.out.println("mehmetttt");// bu komut konsala yazılan metni print eder
+        // dan sonra yazılan ifadeler komut veya kod satırı olarak değil yorum olarak tanımlanır
+        /*
+        bu araya yazılan her ifade yorum satırı olur java kod olarak görmez
+        başarı gayrete aşıktır
+        bahanesi olanın başarısı olmaz
+         */
+
+
+       // System.out.println("meriiiiyy");// çalışmasını istemediğniz kod satırı başına // tırnak yazark yoruma alınır
+        //System.out.println("meriiiiyy");// çalışmasını istemediğniz kod satırı başına // tırnak yazark yoruma alınır
+        //System.out.println("meriiiiyy");// çalışmasını istemediğniz kod satırı başına // tırnak yazark yoruma alınır
+        //System.out.println("meriiiiyy");// çalışmasını istemediğniz kod satırı başına // tırnak yazark yoruma alınır
+        //System.out.println("meriiiiyy");// çalışmasını istemediğniz kod satırı başına // tırnak yazark yoruma alınır
+
+
+
+
+
+        System.out.print("merve yıldırım : ");
+        System.out.println("28");
+        System.out.println("aksaray");
+        System.out.println("memur");
+
+
+
+
+    }
+}
---
+++ src/j02_DataTypes_WrapperClass/C01_VariableCreate.java
@@ -0,0 +1,29 @@
+package j02_DataTypes_WrapperClass;
+
+public class C01_VariableCreate {
+
+    public static void main(String[] args) {
+        // 1. yol -> best practice
+
+        int yas =48;
+        int maas=30000;
+        System.out.println(yas);//48
+        System.out.println("yas");//28
+        System.out.println("benim maas :"+maas);
+
+        int yevmiye , age , kilo;
+        age=28;
+        yevmiye=2555;
+        kilo=255;
+        System.out.println(age);
+        System.out.println("benim hepsi  " +yevmiye);
+
+        
+    }
+
+
+
+
+
+
+}
